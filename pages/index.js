4b189e167cc66fd997b82c0aa588056a1dcae13e
Jonny Ballard <ballardjonny@me.com>
2024-04-25 21:52:57 +0100

    Update tagline

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Ballard Design Studio</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Ballard Design Studio" />
+        <p>Product Design Consultancy</p>
       </main>
 
       <Footer />
