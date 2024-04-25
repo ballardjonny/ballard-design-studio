deb3810a3424fd6d606b6fd719b97ec5bc0552bc
Jonny Ballard <ballardjonny@me.com>
2024-04-25 22:06:06 +0100

    Custom font

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import localFont from 'next/font/local'
+const myFont = localFont({ src: './FKDisplay/FKDisplayTrial-Regular.otf' })
 
 export default function Home() {
   return (
@@ -10,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className={myFont.className}>
         <Header title="Ballard Design Studio" />
         <p>Product Design Consultancy</p>
       </main>
