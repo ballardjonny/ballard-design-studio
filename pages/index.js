2991e8caac62a08a75ba9d14f9f76c181a3bb499
Jonny Ballard <ballardjonny@me.com>
2024-04-25 21:18:15 +0100

    Footer changes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Ballard Design Studio" />
         <h1>Ballard Design Studio</h1>
       </main>
 
